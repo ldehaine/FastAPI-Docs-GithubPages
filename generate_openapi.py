facd470f81bd30e91b1d479a2bf0ba519262f5b8
ldehaine <louyann.dehaine@gmail.com>
2022-03-27 23:48:49 +0200

    simplify generate_openapi.py

--- generate_openapi.py
+++ generate_openapi.py
@@ -6,6 +6,4 @@ from main import my_awesome_api
 
 client = TestClient(my_awesome_api)
 with open("openapi.json", "w") as f:
-    f.write(
-        json.dumps(client.get("/openapi.json").json(), indent=4)
-    )
+    json.dump(client.get("/openapi.json").json(), f, indent=4)
